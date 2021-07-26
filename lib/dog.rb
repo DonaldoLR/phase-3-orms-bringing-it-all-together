51f25c93fb30d211affacec64761849c7fbfd560
github username <github email address>
2021-07-25 22:00:21 -0400

    practice orms and sql queries in ruby

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,87 @@
 class Dog
+  attr_accessor :name, :breed 
+  attr_reader :id
 
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+  ## Instance Methods 
+  def save 
+    if self.id 
+      self.update
+    else 
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].last_insert_row_id
+      self
+    end
+  end 
+
+
+
+
+  ## Class Methods
+  def self.create_table 
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        breed TEXT
+      );
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.create(name:, breed:)
+    new_dog = self.new(name: name, breed: breed)
+    new_dog.save
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+
+    self.new(name: name, breed: breed, id: id)
+  end
+  
+  def self.all 
+    sql = <<-SQL
+      SELECT * FROM dogs
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL 
+      SELECT * FROM dogs 
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
+  end
+
+  def self.find(id)
+    sql = <<-SQL 
+      SELECT * FROM dogs 
+      WHERE id = ?
+    SQL
+    row = DB[:conn].execute(sql, id)[0]
+    self.new_from_db(row)
+  end
 end
